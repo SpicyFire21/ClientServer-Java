fe06dd59f8d2867f870c0fd5bc35afae45270d9a
Adam KHNAGUI <khnagui.adam@gmail.com>
2026-09-09 13:35:50 +0200

    exercice 2

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -7,7 +7,7 @@ class EchoServer  {
  
 	BufferedReader br = null; // pour lire du texte sur la socket
 	PrintStream ps = null; // pour envoyer du texte sur la socket
-	String line = null; // la ligne reçu/envoyée
+
 	ServerSocket conn = null; 
 	Socket sock = null;
 	int port = -1;
@@ -34,18 +34,18 @@ class EchoServer  {
             br = new BufferedReader(new InputStreamReader(sock.getInputStream())); // creation flux lecture lignes de textes
             ps = new PrintStream(sock.getOutputStream()); // création flux écriture lignes de texte
 
-            line = br.readLine(); // réception d'une ligne
-            System.out.println("le client me dit : "+line); // affichage debug
+            String line;
 
-            ps.println(line); // envoi de la ligne précédemmetn reçue
+            while ((line = br.readLine()) != null) {
+                System.out.println("Le client me dit : " + line);
+                ps.println(line);
+            }
 
+            br.close();
+            ps.close();
+            System.out.println("Client déconnecté");
         }
 
-
-
-
-//	    br.close();
-//	    ps.close();
 	}
 	catch(IOException e) {
 	    System.out.println(e.getMessage());
